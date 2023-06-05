4168895312d2dc193c2763f69af4544cd515c560
Animesh Acharya <animeshacharya867+github@gmail.com>
2023-06-05 22:04:29 +0545

    Created a new file for practicing tailwind css

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <>
+      <div className="w-full flex bg-brown h-fit ">
+        <img
+          src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+          className="w-60 rounded-full object-contain"
+        />
+      </div>
+    </>
+  );
+}
